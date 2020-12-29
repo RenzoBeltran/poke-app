8fcd754b1ccd4f030f3e9c62bd14bbc1b0f2d740
Renzo <renzob130@gmail.com>
2020-12-29 17:20:07 -0500

    ADD: firebase with the project

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          If you want to be the best, you have to start today :)
         </p>
         <a
           className="App-link"
